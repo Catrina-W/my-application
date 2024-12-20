12521c163246788c5676c6b2df44bb0ec2039b2e
Catrina-W <catrinawhaley19@gmail.com>
2024-12-19 19:09:36 -0500

    Created local API and 3 cards for Project page

---
+++ src/components/WorkCardData.js
@@ -0,0 +1,26 @@
+import pro1 from "../assets/project1.png"
+import pro2 from "../assets/project2.png"
+import pro3 from "../assets/project3.png"
+
+const ProjectCardData = [
+    {
+        imgsrc: pro1,
+        title: "Ecommerce Website Design",
+        text: "Thriving Facebook community and top-ranked podcast, Amy inspires a grounded, tangible and self-affirming attitude for over 100,000 online entrepreneurs.",
+        view: "https://youtu.be/0h2b4ftbZcU?si=tN3QOWvpuuciQVGw"
+    },
+    {
+        imgsrc: pro2,
+        title: "LMS/Course Website Design",
+        text: "Thriving Facebook community and top-ranked podcast, Amy inspires a grounded, tangible and self-affirming attitude for over 100,000 online entrepreneurs.",
+        view: "https://youtu.be/0h2b4ftbZcU?si=tN3QOWvpuuciQVGw"
+    },
+    {
+        imgsrc: pro3,
+        title: "Ecommerce Design",
+        text: "Thriving Facebook community and top-ranked podcast, Amy inspires a grounded, tangible and self-affirming attitude for over 100,000 online entrepreneurs.",
+        view: "https://youtu.be/0h2b4ftbZcU?si=tN3QOWvpuuciQVGw"
+    }
+];
+
+export default ProjectCardData
--- src/routes/Project.js
+++ src/routes/Project.js
@@ -4,6 +4,7 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import HeroImg2 from '../components/HeroImg2';
 import PricingCard from '../components/PricingCard';
+import Work from '../components/Work';
 
 
 const Project = () => {
@@ -11,6 +12,7 @@ const Project = () => {
     <div>
       <Navbar />
       <HeroImg2 heading="PROJECTS" text="Some of my most recent work" />
+      <Work />
       <PricingCard />
       <Footer />
     </div>
